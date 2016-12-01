a4b1adf33f10b65ff4c04fc6b99e6ebe46cf00db
Valentin Roussellet <louen@palouf.org>
2016-12-01 17:51:33 +0100

    Added doc to britghtness script

--- linux/brightness/brightness.cpp
+++ linux/brightness/brightness.cpp
@@ -1,19 +1,38 @@
+/* A small program to replace xbacklight which does not
+ * work on my system. Should run suid root if you want
+ * to actually modify the brightness.
+ * Compile with -std=c++11
+ */
+
+/* Usage :
+ * 'brightness' will print the current and max brightness
+ *  (does't require root privileges)
+ * 'brightness 42' sets the brightness level to 42
+ * 'brightness +12' increase the brightness level by 12
+ * 'brightness -20' decrease the brightness level by 20
+ *
+ * Brightness will be capped between 0 and the max allowed value
+ */
+
 #include <cstdio>
 #include <cstdlib>
-
 #include <string>
 #include <iostream>
 
+// The /sys/ subfolder corresponding to the backlight control.
 const char* g_folder = "/sys/class/backlight/intel_backlight";
 
+// Command-line parsing structures
 enum Status { PRINT, ABSOLUTE, PLUS, MINUS, ERROR };
 
-struct Args {
-    Status mode;
-    int value;
+struct Args
+{
+    Status mode; // Which action to perform
+    int value;   // Value of the argument
 };
 
-Args parse( int argc, char**argv)
+// Parse the arguments
+Args parse( int argc, char**argv )
 {
     Args result;
     result.mode = ERROR;
@@ -37,7 +56,7 @@ Args parse( int argc, char**argv)
     return result;
 }
 
-
+// Read a value from a given file
 int read_value( const char* endpath )
 {
     std::string filename(g_folder);
@@ -53,20 +72,22 @@ int read_value( const char* endpath )
         exit(EXIT_FAILURE);
     }
 
-    // Read value and close the file 
+    // Read value and close the file
     int result = -1;
     int ok = fscanf(file, "%d", &result);
     fclose(file);
 
     if (!ok)
-    { 
+    {
         std::cerr<<"Error : cannot read from "<<endpath<<std::endl;
+        perror("read_value");
         exit(EXIT_FAILURE);
     }
     return result;
 }
 
-void write_value( const char* endpath, int value)
+// Write value to a given file (will require root)
+void write_value( const char* endpath, int value )
 {
     std::string filename(g_folder);
     filename+="/";
@@ -82,7 +103,7 @@ void write_value( const char* endpath, int value)
 
     int ok = fprintf( file, "%d", value);
     fclose(file);
-    if (!ok){ 
+    if (!ok){
         std::cerr<<"Error : cannot write to "<<endpath<<std::endl;
         exit(EXIT_FAILURE);
     }
@@ -90,16 +111,26 @@ void write_value( const char* endpath, int value)
 
 void print_usage()
 {
+    printf(
+            "Usage :\n"
+            " 'brightness' will print the current and max brightness\n"
+            " (does't require root privileges)\n"
+            " 'brightness 42' sets the brightness level to 42\n"
+            " 'brightness +12' increase the brightness level by 12\n"
+            " 'brightness -20' decrease the brightness level by 20\n"
+            "\n"
+            " Brightness will be capped between 0 and the max allowed value\n");
 }
 
-
-int clamp ( int value, int min, int max)
+// clamp a value between min and max
+int clamp ( int value, int min, int max )
 {
     return std::min(max, std::max(min, value));
 }
 
-int main( int argc, char** argv)
+int main( int argc, char** argv )
 {
+    // Parse args and read values
     const Args args = parse(argc,argv);
     const int current = read_value("actual_brightness");
     const int max = read_value("max_brightness");
@@ -114,11 +145,11 @@ int main( int argc, char** argv)
 
         case PLUS:
         case MINUS: // args.value is negtive in this case.
-            newval = clamp( current + args.value, 0, max);
+            newval = clamp(current + args.value, 0, max);
             write_value("brightness", newval);
             break;
         case ABSOLUTE:
-            newval = clamp( args.value, 0, max);
+            newval = clamp(args.value, 0, max);
             write_value("brightness", newval);
             break;
 
@@ -127,7 +158,6 @@ int main( int argc, char** argv)
             break;
     }
 
+    // Return 0 if everything went fine, and 1 if not.
     return args.mode == ERROR;
 }
-
-
